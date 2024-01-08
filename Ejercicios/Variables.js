81fadc7e557df09c69d4e423e6d8d9859ed29528
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-01-08 10:26:38 -0300

    aprendiendo a usar variables y agregando el js al html

---
+++ Ejercicios/Variables.js
@@ -0,0 +1,9 @@
+/*
+Tipos de datos:
+undefined,null,boolean,string,symbol,number, y object.
+*/
+var mivariable = "freeCodeCamp";
+var mivariable2 = 16;
+var miNombre = "Emanuel"; //se usa CamelCase
+console.log(mivariable);
+console.log(mivariable2);
